af8e1eea6f53ce8be5cd8b32d2ff0f4effb0f7b3
daniel tea <daniel.l.tea@gmail.com>
2023-02-03 18:50:44 -0800

    add rhf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+  const onSubmit = (data) => console.log(data)
+
+  console.log(watch('example')) // watch input value by passing the name of it
+
   return (
     <main>
-      <form></form>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input defaultValue="test" {...register('example')} />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input {...register('exampleRequired', { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
     </main>
   )
 }
