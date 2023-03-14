bc601d9009e709767e62982b7ff2243a821835aa
daniel tea <daniel.l.tea@gmail.com>
2023-03-13 22:05:56 -0700

    credentials login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,52 @@
 import NextAuth from 'next-auth'
-import { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import { NextAuthOptions, User } from 'next-auth'
 
 export const authOptions: NextAuthOptions = {
-  providers: [],
+  providers: [
+    CredentialsProvider({
+      id: 'credentials-provider',
+      name: 'Credentials',
+      // this object is used to generate a form on sign in page
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+
+      async authorize(credentials, req) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        // const res = await fetch('/your/endpoint', {
+        //   method: 'POST',
+        //   body: JSON.stringify(credentials),
+        //   headers: { 'Content-Type': 'application/json' },
+        // })
+        // const user = await res.json()
+
+        // If no error and we have user data, return it
+        // if (res.ok && user) {
+        //   return user
+        // }
+        // // Return null if user data could not be retrieved
+        // return null
+
+        if (credentials?.username === 'admin') {
+          const user: User = {
+            id: '123',
+            name: 'admin',
+            email: 'admin@website.com',
+          }
+          return user
+        }
+
+        return null
+      },
+    }),
+  ],
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: 'jwt',
